1b068d71b1b1a0a678f5d1b264f42638f6e9735f
Omar Mohamed <omer.eldanin@gmail.com>
2025-12-15 15:18:27 +0200

    edit

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -81,6 +81,8 @@ export class UsersController {
     if (file) {
       data.avatar = "uploads/" + file.filename; // or save full path if you want
     }
+    console.log(data);
+
     const user = await this.userService.updateProfile(loggedInUser.id, data);
     return {
       message: "success",
